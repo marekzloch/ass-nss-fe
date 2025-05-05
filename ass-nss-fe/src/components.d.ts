6d65e7ed3fe6965d76fb12a4e3edd7f43c3f385b
Marek Zloch <56075692+MZ-2406@users.noreply.github.com>
2025-05-05 19:24:16 +0200

    Prepare CameraImages component and extract logic for date formating

--- ass-nss-fe/src/components.d.ts
+++ ass-nss-fe/src/components.d.ts
@@ -9,6 +9,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./components/AppFooter.vue')['default']
+    CameraImages: typeof import('./components/CameraImages.vue')['default']
     Graph: typeof import('./components/Graph.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
