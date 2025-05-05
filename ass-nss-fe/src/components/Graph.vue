4670d44a839781f095707e883a514db6b9674a17
Marek Zloch <56075692+MZ-2406@users.noreply.github.com>
2025-05-05 19:25:05 +0200

    Adjust Graph component

--- ass-nss-fe/src/components/Graph.vue
+++ ass-nss-fe/src/components/Graph.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
 import { Chart, Grid, Line, Responsive, Tooltip } from 'vue3-charts'
 import {computed} from "vue";
+import type { Measurement } from '@/pages/index.vue';
+import { formatCzechDate, formatCzechTime } from '@/helpers/stringFormatters';
 
-const props = defineProps(['data'])
-
-function formatCzechDate(isoString) {
-  const date = new Date(isoString);
-
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${day}. ${month}. ${year}`;
+type Props = {
+  measurements: Measurement[];
+  selectedMeasurement: Measurement | null;
 }
 
-function formatCzechTime(isoString) {
-  const date = new Date(isoString);
+const props = defineProps<Props>();
 
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-
-  return `${hours}:${minutes}:${seconds}`;
-}
-
-function xLabel(isoString){
+function xLabel(isoString: string) {
   const date = new Date(isoString);
 
   const hours = date.getHours().toString().padStart(2, '0');
@@ -33,32 +20,33 @@ function xLabel(isoString){
   return `${hours}:${minutes}`;
 }
 
-const graphData = computed(()=>{
-  return props.data.map( d => {
+const graphData = computed(() => {
+  return props.measurements.map(d => {
     return {
+      id: d.id,
       acoustic: d.acoustic,
       date: formatCzechDate(d.created_at),
       time: formatCzechTime(d.created_at),
-      timestamp: new Date(d.created_at).getTime()
+      timestamp: new Date(d.created_at).getTime(),
     }
   })
 })
 
 const margin = ref({
   left: 0,
-  top: 0,
+  top: 10,
   right: 0,
   bottom: 0
 })
 
 const axis = ref({
   primary: {
-  },
-  secondary: {
     type: 'band',
-    format: (val) => {
+    format: (val: string) => {
       return xLabel(val)
     }
+  },
+  secondary: {
   }
 })
 
@@ -67,33 +55,40 @@ const axis = ref({
 <template>
   <Responsive class="w-full">
     <template #main="{ width }">
-  <Chart
-    :size="{ width: width, height: 420 }"
-    :data="graphData"
-    :margin="margin"
-    direction="vertical"
-    :axis="axis"
-  >
 
-    <template #layers>
-      <Grid strokeDasharray="2,2" />
-      <Line :dataKeys="['acoustic', 'timestamp']" />
-    </template>
+      <Chart 
+        :size="{ width: width, height: 420 }" 
+        :data="graphData" 
+        :margin="margin" 
+        direction="horizontal"
+        :axis="axis">
 
-    <template #widgets>
-      <Tooltip
-        borderColor="#48CAE4"
-        :config="{
-          timestamp: {color : 'transparent'}
-        }"
-      />
-    </template>
+        <template #layers>
 
-  </Chart>
+          <Grid strokeDasharray="2,2" />
+
+          <Line :dataKeys="['timestamp', 'acoustic']" />
+
+        </template>
+
+        <template #widgets>
+
+          <Tooltip 
+            borderColor="#48CAE4" 
+            :config="{
+              timestamp: { color: 'transparent' },  
+              id: { color: 'transparent' },         
+              acoustic: {
+                label: 'Akustická emise',
+                format: (val) => `${val.toFixed(1)}`
+              },
+              time: { label: 'Čas' },
+              date: { label: 'Datum' }
+            }" />
+
+        </template>
+
+      </Chart>
     </template>
   </Responsive>
 </template>
-
-<style scoped>
-
-</style>
