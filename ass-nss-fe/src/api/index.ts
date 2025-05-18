39a9f4572ef50fbf94393960bea5248d4ea15cf7
Marek Zloch <56075692+MZ-2406@users.noreply.github.com>
2025-05-18 21:53:02 +0200

    Document api related files

--- ass-nss-fe/src/api/index.ts
+++ ass-nss-fe/src/api/index.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+// this is the api client that is used to communicate with the backend
+// it is created using axios and has base url set to the backend url 
+// with current BE implementation it is set to the ngrok url that changes 
+// on every restart 
+
+// headers are set to skip browser warning -> after deploying to production this header should be removed
+
+
 const apiClient = axios.create({
     baseURL: 'https://d71f-89-248-248-167.ngrok-free.app',
     headers: {
